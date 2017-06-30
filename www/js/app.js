bdcc09cb6b85f651a2e9bd44e774f4c3c5449a63
Animesh Kumar <animesh0721@gmail.com>
2017-06-30 13:09:03 +0530

    Adds paypal cordova plugin to checkout with paypal

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services', 'appConstant'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -30,6 +30,7 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter
       templateUrl: 'templates/map.html',
       controller: 'MapCtrl'
     });
+
 
   $urlRouterProvider.otherwise('/');
 
--- www/js/controller.js
+++ www/js/controller.js
@@ -1,90 +1,63 @@
 angular.module('starter.controllers', [])
 
-  .controller('MapCtrl', function ($scope, $state, $cordovaGeolocation, $cordovaBackgroundGeolocation) {
+.controller('MapCtrl', function ($scope, $state, $cordovaGeolocation, PaypalService, $ionicPopup) {
 
-    $scope.lat = 0;
-    $scope.lng = 0;
+  $scope.lat = 0;
+  $scope.lng = 0;
 
-    var options = {timeout: 10000, enableHighAccuracy: true};
+  $scope.total = function () {
+    return 0.01;
+  };
 
-    $scope.start = function () {
-      console.log('inside start tracking');
-
-      // Background tracking
-      // var config = {
-      //   desiredAccuracy: 0,
-      //   stationaryRadius: 20,
-      //   distanceFilter: 10,
-      //   debug: true,
-      //   interval: 2000
-      // };
-      // document.addEventListener("deviceready", function () {
-      //   console.log('device ready hai');
-      //   $cordovaBackgroundGeolocation.configure(config)
-      //     .then(
-      //       null,
-      //       function (error) {
-      //         console.log(error);
-      //       },
-      //       function (location) {
-      //         console.log('BackgroundLocation: (' + location.latitude +
-      //           ', ' + location.longitude + ')');
-      //
-      //         $scope.lat = location.latitude;
-      //         $scope.lng = location.longitude;
-      //
-      //       }
-      //     );
-      //
-      //
-      //   // Turn ON the background-geolocation system.
-      //   $cordovaBackgroundGeolocation.start();
-      // //
-      // }, false);
-      //
-      // Foreground tracking
-      var options = {
-        frequency: 3000,
-        enableHighAccuracy: false
-      };
-      $scope.watch_position = $cordovaGeolocation.watchPosition(options);
-      console.log('Watching with ID: ', $scope.watch_position.watchID);
-      $scope.watch_position
-        .then(
-          null,
-          function (error) {
+  $scope.start = function () {
+    // Foreground tracking
+    var options = {
+      frequency: 3000,
+      enableHighAccuracy: false
+    };
+    $scope.watch_position = $cordovaGeolocation.watchPosition(options);
+    console.log('Watching with ID: ', $scope.watch_position.watchID);
+    $scope.watch_position
+      .then(
+        null,
+        function (error) {
           console.log(error);
-          },
-          function (position) {
+        },
+        function (position) {
           console.log(position);
           $scope.lat = position.coords.latitude;
           $scope.lng = position.coords.longitude;
-          });
-    };
-    // $scope.watch_position.clearWatch();
+        });
+  };
 
-    $scope.stop = function () {
-      console.log('Inside stop, watchID: ', $scope.watch_position.watchID);
-      $cordovaGeolocation.clearWatch($scope.watch_position.watchID);
-        // .then(function (result) {
-        //   console.log('Tracking ruk gai.');
-        // }, function (error) {
-        //   console.log('Tracking rukne mein error aa rahi hai: ', error);
-        // });
-      // $cordovaBackgroundGeolocation.stop();
-    };
-
-    $cordovaGeolocation.getCurrentPosition(options)
-      .then(function (position) {
-        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        var mapOptions = {
-          center: latLng,
-          zoom: 15,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-        console.log('created google map');
+  $scope.stop = function () {
+    // console.log('Inside stop, watchID: ', $scope.watch_position.watchID);
+    // $cordovaGeolocation.clearWatch($scope.watch_position.watchID);
+    PaypalService.initPaymentUI().then(function () {
+      PaypalService.makePayment($scope.total(), "Total").then(function (value) {
+        console.log(value);
       }, function (error) {
         console.error(error);
       });
-  });
+    }, function (error) {
+      console.error(error);
+    });
+  };
+
+  $scope.payPal = function () {
+    PaypalService.initPaymentUI().then(function () {
+      console.log('paypal ui initiated');
+      PaypalService.makePayment($scope.total(), "Total").then(function (value) {
+        console.log(value);
+        var alertPopUp = $ionicPopup.alert({
+          title: 'Payment Successful',
+          template: 'Payment ID: ' + value['response']['id']
+        });
+      }, function (error) {
+        console.error(error);
+      });
+    }, function (error) {
+      console.error(error);
+    });
+  };
+});
